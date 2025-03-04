915cfb72628c9eff48e498cd8cbe858ae0f2f9ca
galalmagdy <70648646+galalmagdy@users.noreply.github.com>
2025-03-04 16:08:01 +0200

    Add floating Search Button

--- app/src/main/java/com/example/mletask/presentation/ui/HomeScreen.kt
+++ app/src/main/java/com/example/mletask/presentation/ui/HomeScreen.kt
@@ -1,5 +1,9 @@
 package com.example.mletask.presentation.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,19 +14,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -41,7 +49,7 @@ import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
 import kotlinx.coroutines.delay
 
-@OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalPagerApi::class, ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
     val experiences by viewModel.experiences.collectAsState()
@@ -50,106 +58,183 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
 
     var mainSearchQuery by remember { mutableStateOf("") }
     var showDialog by remember { mutableStateOf(false) }
-
     var showExperienceDialog by remember { mutableStateOf(false) }
     var selectedExperienceId by remember { mutableStateOf("") }
 
-    // Create a PagerState to control the HorizontalPager
     val pagerState = rememberPagerState()
 
-    // LaunchedEffect to handle auto-scrolling
     LaunchedEffect(pagerState) {
         while (true) {
-            // Delay between scrolls (e.g., 2 seconds)
             delay(4000)
-
-            // Calculate the next page
-            val nextPage = if (pagerState.currentPage == recommendedExperiences.size - 1) {
-                // If at the end, start scrolling backward
-                pagerState.currentPage - 1
-            } else {
-                // Otherwise, scroll forward
-                pagerState.currentPage + 1
-            }
-
-            // Animate to the next page
+            val nextPage = if (pagerState.currentPage == recommendedExperiences.size - 1) 0 else pagerState.currentPage + 1
             pagerState.animateScrollToPage(nextPage.coerceIn(0, recommendedExperiences.size - 1))
         }
     }
 
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        // Search Bar
-        item {
-            OutlinedTextField(
-                value = mainSearchQuery,
-                onValueChange = { mainSearchQuery = it },
-                modifier = Modifier.fillMaxWidth(),
-                placeholder = { Text("Search experiences...") },
-                singleLine = true,
-                leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search Icon") },
-                trailingIcon = {
-                    IconButton(onClick = {
-                        showDialog = true
-                        if (mainSearchQuery.isNotBlank()) {
-                            viewModel.searchExperiences(mainSearchQuery)
-                        }
-                    }) {
-                        Icon(Icons.AutoMirrored.Filled.Send, contentDescription = "Filter Icon")
+    val scrollState = rememberLazyListState()
+    val isSearchBarVisible = remember { derivedStateOf { scrollState.firstVisibleItemIndex == 0 } }
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            state = scrollState,
+            modifier = Modifier.fillMaxSize().background(Color.White)
+        ) {
+            // Sticky Header for Search Bar
+            stickyHeader {
+                AnimatedVisibility(
+                    visible = isSearchBarVisible.value,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.White)
+                    ) {
+                        TextField(
+                            value = mainSearchQuery,
+                            onValueChange = { mainSearchQuery = it },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp),
+                            placeholder = { Text("Search experiences...") },
+                            singleLine = true,
+                            leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search Icon") },
+                            trailingIcon = {
+                                IconButton(onClick = {
+                                    showDialog = true
+                                    if (mainSearchQuery.isNotBlank()) {
+                                        viewModel.searchExperiences(mainSearchQuery)
+                                    }
+                                }) {
+                                    Icon(Icons.AutoMirrored.Filled.Send, contentDescription = "Filter Icon")
+                                }
+                            },
+                            colors = TextFieldDefaults.textFieldColors(
+                                focusedIndicatorColor = Color.Transparent,
+                                unfocusedIndicatorColor = Color.Transparent,
+                                disabledIndicatorColor = Color.Transparent,
+                                containerColor = Color.Transparent
+                            )
+                        )
                     }
-                },
-                colors = TextFieldDefaults.outlinedTextFieldColors(containerColor = Color.Transparent)
-            )
+                }
+            }
 
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Welcome!", fontSize = 22.sp, fontWeight = FontWeight.Bold)
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = "Now you an explore any experience in 360 degrees and get all the details about it all in one place",
-                fontSize = 14.sp,
-                fontWeight = FontWeight.Bold)
-        }
+            // Welcome Section
+            item {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .background(Color.White)
+                ) {
+                    Text(text = "Welcome!", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(4.dp).background(Color.White))
+                    Text(
+                        text = "Now you can explore any experience in 360 degrees and get all the details about it all in one place",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
 
-        // Recommended Experiences
-        item {
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Recommended Experiences", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+            // Recommended Experiences
+            item {
+                Spacer(modifier = Modifier.height(8.dp).background(Color.White))
+                Text(
+                    text = "Recommended Experiences",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
 
-            HorizontalPager(
-                count = recommendedExperiences.size,
-                state = pagerState, // Pass the PagerState here
-                modifier = Modifier.fillMaxWidth().height(250.dp)
-            ) { page ->
-                ExperienceItem(
-                    experience = recommendedExperiences[page],
-                    onClick = {
-                        selectedExperienceId = recommendedExperiences[page].id
-                        showExperienceDialog = true
-                    },
-                    onLike = { viewModel.likeExperience(recommendedExperiences[page].id) }
+                HorizontalPager(
+                    count = recommendedExperiences.size,
+                    state = pagerState,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(250.dp)
+                ) { page ->
+                    ExperienceItem(
+                        experience = recommendedExperiences[page],
+                        onClick = {
+                            selectedExperienceId = recommendedExperiences[page].id
+                            showExperienceDialog = true
+                        },
+                        onLike = { viewModel.likeExperience(recommendedExperiences[page].id) }
+                    )
+                }
+            }
+
+            // Most Recent Experiences
+            item {
+                Spacer(modifier = Modifier.height(8.dp).background(Color.White))
+                Text(
+                    text = "Most Recent",
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
                 )
             }
 
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(text = "Most Recent", fontSize = 22.sp, fontWeight = FontWeight.Bold)
+            items(experiences) { experience ->
+                ExperienceItem(
+                    experience = experience,
+                    onClick = {
+                        selectedExperienceId = experience.id
+                        showExperienceDialog = true
+                    },
+                    onLike = { viewModel.likeExperience(experience.id) }
+                )
+            }
         }
 
-        // Most Recent Experiences
-        items(experiences) { experience ->
-            ExperienceItem(
-                experience = experience,
-                onClick = {
-                    selectedExperienceId = experience.id
-                    showExperienceDialog = true
-                },
-                onLike = { viewModel.likeExperience(experience.id) }
-            )
+        // Floating Search Bar
+        AnimatedVisibility(
+            visible = !isSearchBarVisible.value,
+            enter = fadeIn(),
+            exit = fadeOut(),
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 16.dp)
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+                    .background(Color.Transparent),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                TextField(
+                    value = mainSearchQuery,
+                    onValueChange = { mainSearchQuery = it },
+                    modifier = Modifier.fillMaxWidth().background(Color.White),
+                    placeholder = { Text("Search experiences...") },
+                    singleLine = true,
+                    leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search Icon") },
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            showDialog = true
+                            if (mainSearchQuery.isNotBlank()) {
+                                viewModel.searchExperiences(mainSearchQuery)
+                            }
+                        }) {
+                            Icon(Icons.AutoMirrored.Filled.Send, contentDescription = "Filter Icon")
+                        }
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        containerColor = Color.Transparent
+                    )
+                )
+            }
         }
     }
 
-    // 🔹 Experience Details Dialog (Opens when an experience is clicked)
+    // 🔹 Experience Details Dialog
     if (showExperienceDialog) {
         mainSearchQuery = ""
         ExperienceDetailsDialog(
@@ -189,7 +274,7 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
                 }
 
                 // Search Bar
-                OutlinedTextField(
+                TextField(
                     value = searchQuery,
                     onValueChange = {
                         searchQuery = it
@@ -199,10 +284,15 @@ fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
                     placeholder = { Text("Search experiences...") },
                     singleLine = true,
                     leadingIcon = { Icon(Icons.Default.Search, contentDescription = "Search Icon") },
-                    colors = TextFieldDefaults.outlinedTextFieldColors(containerColor = Color.Transparent)
+                    colors = TextFieldDefaults.textFieldColors(
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent,
+                        containerColor = Color.Transparent
+                    )
                 )
 
-                Spacer(modifier = Modifier.height(8.dp))
+                Spacer(modifier = Modifier.height(8.dp).background(Color.White))
 
                 LazyColumn(
                     modifier = Modifier.weight(1f)
